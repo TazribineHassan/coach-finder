fe9006c973504aa86633586aa5d648c238c918e8
TazribineHassan <tazribinehassan1998@gmail.com>
2021-07-25 20:07:01 +0100

    first commit

---
+++ src/pages/coaches/CoachesList.vue
@@ -0,0 +1,18 @@
+<template>
+    <section>
+        Filter
+    </section>
+    <section>
+        List of COACHES
+    </section>
+</template>
+
+<script> 
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/router.js
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import CoachesList from  './pages/coaches/CoachesList.vue';
+import CoacheDetails from  './pages/coaches/CoacheDetails.vue';
+import CoachRegistration from  './pages/coaches/CoachRegistration.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestsRecieves from './pages/requests/RequestsRecieves.vue';
+import NotFound from './pages/NotFound.vue';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes:[
+        {path: '/', redirect:'/coaches'},
+        {path: '/coaches', component: CoachesList},
+        {path: '/coaches/:id', component: CoacheDetails, children:[
+            {path: 'contact', component: ContactCoach},
+        ]},
+        {path: '/register', component: CoachRegistration},
+        {path: '/requests', component: RequestsRecieves},
+        {path: '/:notFound(.*)', component: NotFound},
+    ],
+})
+
+export default router;
